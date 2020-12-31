07b61260613950998e8602bbfefe06de9be4d8bb
luispaulo21 <luispaulocavalcante20@gmail.com>
2020-12-31 20:10:01 -0300

    Adicionado controle de miniaturas

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const valorDoProduto = 202.25;
 const areaValorDoProduto =  document.querySelector(".secao__produto .produto__valor");
+const miniaturasDoProduto = document.querySelectorAll(".produto__miniatura .miniatura__imagem");
+const imagemDoProduto = document.querySelector(".produto__imagem .imagem__principal");
+
+miniaturasDoProduto.forEach((item, index) => {
+	item.addEventListener("click", () => {
+		if(index === 0 ) {
+			imagemDoProduto.setAttribute("src", "img1.png");
+		} else if(index === 1 ) {
+			imagemDoProduto.setAttribute("src", "img2.png");
+		} else if(index === 2 ) {
+			imagemDoProduto.setAttribute("src", "img3.png");
+		} else {
+			imagemDoProduto.setAttribute("src", "img4.png");
+		}
+
+		imagemDoProduto.classList.add("animImg");
+		setTimeout(() => {
+			imagemDoProduto.classList.remove("animImg");
+		}, 300);
+	})
+});
 
 
 const btnQuantidadeMenos = document.querySelector(".produto__quantidade .menos");
@@ -15,7 +36,6 @@ btnQuantidadeMenos.addEventListener("click", () => {
 	}
 	quantidadeDoProduto.innerText = contador;
 	areaValorDoProduto.innerText = `R$ ${valorAtualizado.toString().replace(".", ",")}`;
-	// console.log(contador);
 });
 
 btnQuantidadeMais.addEventListener("click", () => {
@@ -23,9 +43,4 @@ btnQuantidadeMais.addEventListener("click", () => {
 	let valorAtualizado = valorDoProduto * contador;
 	quantidadeDoProduto.innerText = contador;
 	areaValorDoProduto.innerText = `R$ ${valorAtualizado.toString().replace(".", ",")}`;
-	// console.log(contador);
-}); 
-
-quantidadeDoProduto.addEventListener("keypress", () => {
-	
 });
